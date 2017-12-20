a495f98f365cc4814275b31256a4b8a2fb091ea5
kirill3333 <kirill3333@inbox.ru>
2017-12-20 21:36:13 +0300

    split to chunks

--- server/template.js
+++ server/template.js
@@ -5,11 +5,12 @@ export const renderTemplate = appHtml => {
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>React PWA</title>
+            <link rel="stylesheet" href="static/css/main.css">
         </head>
         <body>
             <div id="app">${appHtml}</div>
+            <script src="static/vendor.js"></script>
             <script src="static/app.js"></script>
-            <link rel="stylesheet" href="static/css/main.css">
         </body>
         </html>
    `
